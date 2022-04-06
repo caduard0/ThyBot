7898c3e91e5444c18a13d2515f2979505bd45ecf
Aikenpil <gustebas@hotmail.com>
2022-04-06 20:09:57 -0300

    Discord Bot Files

---
+++ Commands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Webhook;
+using Discord;
+using Discord.Commands;
+using System.Runtime.InteropServices;
+using System.IO;
+using Discord.Audio;
+using Discord.Audio.Streams;
+using System.Media;
+
+namespace bote.modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null)
+            {
+                await Context.Channel.SendMessageAsync("User must be in a voice channel, " +
+                "or a voice channel must be passed as an argument."); return;
+            }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            await channel.ConnectAsync(false, false, true);
+        }
+
+        [Command("exit", RunMode = RunMode.Async)]
+        public async Task InjoinChannel(IVoiceChannel channel = null)
+        {
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+
+            await channel.DisconnectAsync();
+
+        }
+
+        [Command("rojao", RunMode = RunMode.Async)]
+        public async Task rojao(IVoiceChannel channel = null) {
+            await JoinChannel(channel);
+
+            var mymusic = new SoundPlayer();
+            mymusic.SoundLocation = @"C:\Users\guste\Desktop\ConsoleApp1\rojao-estourado.wav";
+            mymusic.PlaySync();
+
+            await ReplyAsync("TARRARRRRRRRRRRRRRSTASATAATTATATATATTAAPOW POW OPWPWPOPWWWPOWWWWWWWWWWW");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace bote
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            _client.Log += Log;
+
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = File.ReadAllText("token.txt");
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += CommandHandler;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task CommandHandler(SocketMessage msg)
+        {
+            //variables
+            string command = "";
+            int lengthOfCommand = -1;
+            var message = msg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+
+            if (msg.Author.IsBot) //This ignores all commands from bots
+                return;
+
+            if (msg.Content.Contains(' '))
+                lengthOfCommand = msg.Content.IndexOf(' ');
+            else
+                lengthOfCommand = msg.Content.Length;
+
+            command = msg.Content.Substring(1, lengthOfCommand - 1).ToLower();
+
+            //Commands without prefix
+            if (msg.Author.Id.Equals(215476850141429761))
+            {
+                await msg.Channel.SendMessageAsync("o heitor");
+            }
+
+            //Commands with prefix
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+
+        }
+    }
+}
